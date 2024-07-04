dacf0bdfa0e1d4677a211b798ad9151f2166639d
mvv712 <mvv712@mail.ru>
2024-07-04 08:56:51 +0300

    Исправлены проверки линтера

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,12 +4,10 @@ import getRandomNumber from '../utils.js';
 const title = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  const divider = [];
-
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
-    };
+    }
   }
 
   return true;
